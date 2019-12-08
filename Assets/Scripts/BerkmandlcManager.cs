65ce369978b1017c0e8ad19289af61412d5735a3
Nina Velikajne <nv6920@student.uni-lj.si>
2019-12-08 11:00:48 +0100

    camera and moving glede na Berkandlca

---
+++ Assets/Scripts/BerkmandlcManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BerkmandlcManager : MonoBehaviour
+{
+
+    private int speed; 
+
+    void Start()
+    {
+        speed = 5;
+    }
+
+   
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        gameObject.transform.position += Movement * speed * UnityEngine.Time.deltaTime;
+
+        Camera mycam = GetComponent<Camera>();
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+    }
+}
