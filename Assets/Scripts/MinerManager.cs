f05c7772a64cc6ef8d128c41224170ad45c99a55
Nina Velikajne <nv6920@student.uni-lj.si>
2019-12-16 09:56:55 +0100

    raycast to miner

--- Assets/Scripts/MinerManager.cs
+++ Assets/Scripts/MinerManager.cs
@@ -11,15 +11,26 @@ public class MinerManager : MonoBehaviour
     private Rigidbody rigidbody;
     private float timer;
     private int direction = 1;
+    private Vector3 lookDirection;
     void Start()
     {
+        
         rigidbody = GetComponent<Rigidbody>();
         timer = changeTime;
+        if (vertical) 
+        {
+            lookDirection = new Vector3(0,0,1);
+        }
+        else 
+        {
+            lookDirection = new Vector3(1,0,0);
+        }
     }
 
     void Update()
     {
         move();
+        look();
     }
 
     void move() 
@@ -29,6 +40,15 @@ public class MinerManager : MonoBehaviour
         {
             direction = -direction;
             timer = changeTime;
+            if (vertical)
+            {
+                Debug.Log(lookDirection.y);
+                lookDirection.z = -lookDirection.z;
+            }
+            else 
+            {
+                lookDirection.x = -lookDirection.x;
+            }
         }
 
         Vector3 position = rigidbody.position;
@@ -43,5 +63,18 @@ public class MinerManager : MonoBehaviour
         }
 
         rigidbody.MovePosition(position);
+    }
+
+    void look() 
+    {
+        Debug.DrawRay(transform.position, lookDirection * 5, Color.green);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, lookDirection * 5, out hit, 3f, LayerMask.GetMask("Berkmandlc"))) 
+        {
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+            }
+        }
     }
 }
